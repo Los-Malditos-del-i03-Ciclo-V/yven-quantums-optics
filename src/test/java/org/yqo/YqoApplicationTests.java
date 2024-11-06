ff9735f7ce246252f4b8f74621a4ee5abd661f85
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-11-05 20:07:52 -0500

    feat(project): Inicializa configuración de Spring Boot
    Se inicializa la configuración del proyecto Spring Boot, incluyendo el archivo pom.xml, la estructura de carpetas y la configuración de compilación.

---
+++ src/test/java/org/yqo/YqoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yqo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YqoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
