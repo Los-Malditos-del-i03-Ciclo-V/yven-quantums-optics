2c2c8c04263dd52102188ec14ff88757180fff06
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-12-05 21:40:38 -0500

    chore(architecture): se elimina toda la arquitectura para una nueva

--- repository/src/main/java/org/yqo/persistence/entity/operador/OperadorRepository.java
+++
@@ -1,9 +0,0 @@
-package org.yqo.persistence.entity.operador;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.yqo.model.operador.entity.OperadorEntity;
-
-@Repository
-public interface OperadorRepository extends JpaRepository <OperadorEntity, Integer> {
-}
