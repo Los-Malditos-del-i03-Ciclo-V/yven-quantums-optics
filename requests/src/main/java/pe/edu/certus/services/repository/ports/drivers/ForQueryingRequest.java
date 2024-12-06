a4c4e036b2f61da682293a4a79553ac568ffac7b
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-12-05 21:46:36 -0500

    chore(frontend): se coloca módulo único para frontend

--- requests/src/main/java/pe/edu/certus/services/repository/ports/drivers/ForQueryingRequest.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.certus.services.repository.ports.drivers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.certus.services.repository.entity.RequestEntity;
-
-@Repository
-public interface ForQueryingRequest extends JpaRepository<RequestEntity, Long> {
-}
